0048f48f88154ab28cfdc8e945b6ebcc6ef3c827
wduf <wduf02@gmail.com>
2025-12-15 08:13:56 -0500

    reorder section exp > proj > skill > cert > edu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import Certifications from './components/Certifications'
-import Container from './components/Container'
-import Education from './components/Education'
-import Experience from './components/Experience'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Spacer from './components/Spacer'
+import Certifications from "./components/Certifications"
+import Container from "./components/Container"
+import Education from "./components/Education"
+import Experience from "./components/Experience"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Spacer from "./components/Spacer"
 
 function App() {
   return (
     <Container>
       <Hero />
       <Spacer />
-      <Skills />
-      <Spacer />
-      <Certifications />
-      <Spacer />
       <Experience />
       <Spacer />
       <Projects />
+      <Spacer />
+      <Skills />
+      <Spacer />
+      <Certifications />
       <Spacer />
       <Education />
       <Spacer />
