213f3e7674be16b9f5a142c9280b09b4a5860d73
wduf <wduf02@gmail.com>
2026-02-15 22:43:04 -0500

    add solid pill bg

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Pill({ children }: Props) {
   return (
-    <span className="text-sm text-neutral-400 border border-neutral-800 rounded-sm px-2">
+    <span className="text-sm text-neutral-400 bg-neutral-900 border border-neutral-900 rounded-sm px-2">
       {children}
     </span>
   )
